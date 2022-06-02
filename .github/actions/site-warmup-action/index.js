09091c22ea5788f31b4045242cdbd1cbb87152d8
giovanidecusati <giovani.decusati@hotmail.com>
2022-06-02 12:52:21 +1000

    post

--- .github/actions/site-warmup-action/index.js
+++ .github/actions/site-warmup-action/index.js
@@ -1,6 +1,7 @@
 
 const core = require('@actions/core');
 const github = require('@actions/github');
+const axios = require('axios');
 
 try {
     // `site-warmup-action` input defined in action metadata file
@@ -11,8 +12,16 @@ try {
     const url = core.getInput('url');
     console.log(`url: ${url}!`);
 
-    const time = (new Date()).toTimeString();
-    core.setOutput("httpresponse", time);
+    axios
+        .get('url')
+        .then(res => {
+            console.log(`statusCode: ${res.status}`);
+            console.log(res);
+            core.setOutput("httpresponse", res);
+        })
+        .catch(error => {
+            console.error(error);
+        });
 
     // Get the JSON webhook payload for the event that triggered the workflow
     const payload = JSON.stringify(github.context.payload, undefined, 2)
